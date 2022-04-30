115d28fdec9ba11dded8d6444228e212828ae578
Ricardo Henrique <ricardo@enesolucoes.com.br>
2022-04-30 11:44:57 -0300

    corrigindo build gastby

--- src/components/TwitterLogin/index.tsx
+++ src/components/TwitterLogin/index.tsx
@@ -5,16 +5,19 @@ import { authTwitter } from '../../services/twitter'
 const TwitterLogin = () => {
   const [hasError, setHasError] = React.useState<boolean>()
   const onClick = async () => {
-    const [result, error] = await tryCatch({ req: authTwitter() });
+    const [result, error] = await tryCatch({ req: authTwitter() })
     if (error) setHasError(true)
     if (result) {
       setHasError(false)
-      console.log('result', result);
+      console.log('result', result)
     }
   }
   return (
     <section className="">
-      <h2>aqui ficara o modal de login no twitter {`${hasError ? 'errrroooo': 'deu bomá}`}</h2>
+      <h2>
+        aqui ficara o modal de login no twitter{' '}
+        {`${hasError ? 'errrroooo' : 'deu bomá'}`}
+      </h2>
       <button onClick={onClick}>Logar</button>
     </section>
   )
