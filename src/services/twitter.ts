cec27d2528694ba9acb5e76783658da7338f8fe8
Ricardo Henrique <ricardo@enesolucoes.com.br>
2022-04-30 11:39:59 -0300

    Setando url de retorno e ajustando requisição do twitter

--- src/services/twitter.ts
+++ src/services/twitter.ts
@@ -3,10 +3,13 @@ import axios from 'axios';
 
 const twitterInstance = axios.create({
   baseURL: process.env.TWITTER_BASE_URL || ''
+  
 })
 export const authTwitter = async ():Promise<void> => {
-  console.log('processs.envasda', process.env.GATSBY_TWITTER_BASE_URL)
   console.log('processs.envasda', process.env.TWITTER_BASE_URL)
-  console.log('processs.envasda', process.env.GATSBY)
-  return await twitterInstance.post(`request_token`);
+ if (typeof window !== undefined) {
+   const { host } = location;
+   const encodedURL = encodeURIComponent(`https://${host}/return-twitter`)
+   return await twitterInstance.post(`request_token?oauth_callback=${encodedURL}`);
+ }
 }
